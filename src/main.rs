1e46a9cd8dcadfbd04157b05840e764cbfbf9930
James Tease <james@jamestease.co.uk>
2018-06-06 16:30:28 +0100

    Post

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ extern crate snowflake;
 extern crate sha2;
 #[macro_use]
 extern crate tera;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
-use actix_web::{ middleware, server, App, HttpRequest, Responder, http, HttpResponse, error, Error, fs };
+use actix_web::{ middleware, server, App, HttpRequest, Responder, http, HttpResponse, error, Error, fs, Form };
 use sha2::{ Sha256, Digest };
 use snowflake::{ create_hash };
 use snowflake::draw::draw;
@@ -38,10 +42,39 @@ fn create_flake(req: HttpRequest<AppState>) -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
+fn show_form(req: HttpRequest<AppState>) -> Result<HttpResponse, Error> {
+    let ctx = tera::Context::new();
 
-fn index(_req: HttpRequest<AppState>) -> impl Responder {
-    "Hi!"
+    let s = req.state().template.render("form.html", &ctx).map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
+
+#[derive(Deserialize)]
+struct Flake {
+    name: String
+}
+
+// use tuple = multiple handlers (automatic)
+fn form_flake(data: (Form<Flake>, HttpRequest<AppState>)) -> Result<HttpResponse, Error> {
+    let mut hasher = Sha256::default();
+    hasher.input(data.0.name.as_bytes());
+    let hash = format!("{:x}", hasher.result());
+
+    // TODO check if file exists already - just return it if so
+    match draw(&hash, &concat!(env!("CARGO_MANIFEST_DIR"), "/images")) {
+        Ok(_) => format!("{:?}", hash),
+        Err(e) => format!("Error creating image: {:?}", e)
+    };
+
+    let mut ctx = tera::Context::new();
+    ctx.add("text", &hash.to_owned());
+    let s = data.1.state().template.render("flake.html", &ctx).map_err(|_| error::ErrorInternalServerError("Template error")).unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+// fn index(_req: HttpRequest<AppState>) -> impl Responder {
+//     "Hi!"
+// }
 
 struct AppState {
     template: tera::Tera
@@ -58,10 +91,14 @@ fn main() {
         App::with_state(AppState{ template: tera })
             .middleware(middleware::Logger::default())
             .handler("/images", fs::StaticFiles::new(concat!(env!("CARGO_MANIFEST_DIR"), "/images")))
-            .resource("/index.html", |r| r.f(|_| "Hello world"))
+            // .resource("/index.html", |r| r.f(|_| "Hello world"))
             // .resource("/flake", |r| r.method(http::Method::GET).f(create_flake))
-            .resource("/flake/{text}", |r| r.method(http::Method::GET).f(create_flake))
-            .resource("/", |r| r.method(http::Method::GET).f(index))
+            .resource("/", |r| {
+                r.method(http::Method::GET).f(show_form);
+                r.post().with(form_flake);
+            })
+            .resource("/{text}", |r| r.get().f(create_flake))
+            // .resource("/", |r| r.method(http::Method::GET).f(index))
     })
         .bind("127.0.0.1:3099")
         .unwrap()
